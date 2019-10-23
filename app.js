f3bda7a61386c0c1b320d6f0f46dbf5ad44bfd85
Thompson Yeboah <45723071+maxblessed@users.noreply.github.com>
2019-10-23 22:30:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-//  new Vue({
-//   el: '#container',
-//   data: {
-//     message: 'Hello Vue!',
-//
-//   },
-//   method:{
-//     chang:function(){
-//       console.log('hi');
-//     }
-//   }
-// })
+
 function getElement(id){
   return document.querySelector(id)
 }
@@ -35,11 +24,7 @@ function calculate(){
     }
 
 let metr=[453.59,16,0.0005,0.45359237,input.value]
-// arr[0]=input.value*gram;
-// arr[1]=input.value*ounce;
-// arr[2]=input.value*tons;
-// arr[3]=input.value;
-// arr[4]=input.value*kilogram;
+
 for(var i=0;i<5;i++){
   if(i!==4){
     arr[i]=metr[i]*input.value;
@@ -57,10 +42,6 @@ update();
 window.oninput=calculate
 function update(){
   var final=JSON.parse(localStorage.getItem('gram'));
-  // getElement('.p:nth-child(2) p').innerHTML=`<span>${final[0]}</span> `;
-  // getElement('.p:nth-child(3) p').innerHTML=`<span>${final[1]}</span> `;
-  // getElement('.p:nth-child(4) p').innerHTML=`<span>${final[2]}</span> `;
-  //   getElement('.p:nth-child(5) p').innerHTML=`<span>${final[4]}</span> `;
   let p=document.getElementsByClassName('local');
   for(var i=0;i<final.length-1;i++){
     p[i].innerHTML=`<span>${final[i]}</span> `;
